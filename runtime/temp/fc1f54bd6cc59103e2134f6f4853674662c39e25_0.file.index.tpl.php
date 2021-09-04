c8afd52a80b6cc54b45986ef800b0a0d195b5b0e
熊成 <920772304@qq.com>
2021-09-04 14:47:52 +0800

    init

--- runtime/temp/fc1f54bd6cc59103e2134f6f4853674662c39e25_0.file.index.tpl.php
+++ runtime/temp/fc1f54bd6cc59103e2134f6f4853674662c39e25_0.file.index.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-09-03 09:35:11
+/* Smarty version 3.1.39, created on 2021-09-04 06:40:22
   from 'D:\phpStudy\PHPTutorial\WWW\demo\view\index\index.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_6131ec4f0d4ac3_25849087',
+  'unifunc' => 'content_613314d64e8224_70259850',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'fc1f54bd6cc59103e2134f6f4853674662c39e25' => 
     array (
       0 => 'D:\\phpStudy\\PHPTutorial\\WWW\\demo\\view\\index\\index.tpl',
-      1 => 1630659352,
+      1 => 1630737620,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_6131ec4f0d4ac3_25849087 (Smarty_Internal_Template $_smarty_tpl) {
+function content_613314d64e8224_70259850 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!doctype html>
 <html lang="en">
 <head>
@@ -31,7 +31,22 @@ function content_6131ec4f0d4ac3_25849087 (Smarty_Internal_Template $_smarty_tpl)
     <title>Document</title>
 </head>
 <body>
-
+    <div>
+        <form action="" method="get" enctype="multipart/form-data">
+            <div>
+                <label>书名：</label>
+                <input type="text" placeholder="书名" name="book_name" id="book_name">
+            </div>
+            <div>
+                <label>搜索引擎：</label>
+                <input type="radio" name="search">笔趣阁
+                <input type="radio" name="search">新笔趣阁
+            </div>
+            <div>
+                <input type="button" value="搜索" id="btn_search" name="btn_search">
+            </div>
+        </form>
+    </div>
 </body>
 </html><?php }
 }
--- view/index/index.tpl
+++ view/index/index.tpl
@@ -8,6 +8,21 @@
     <title>Document</title>
 </head>
 <body>
-
+    <div>
+        <form action="" method="get" enctype="multipart/form-data">
+            <div>
+                <label>书名：</label>
+                <input type="text" placeholder="书名" name="book_name" id="book_name">
+            </div>
+            <div>
+                <label>搜索引擎：</label>
+                <input type="radio" name="search">笔趣阁
+                <input type="radio" name="search">新笔趣阁
+            </div>
+            <div>
+                <input type="button" value="搜索" id="btn_search" name="btn_search">
+            </div>
+        </form>
+    </div>
 </body>
 </html>
